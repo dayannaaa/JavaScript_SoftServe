69aef732c9820071a665af3bb92569545d46d177
lopatiinaaa <dilopatiinaaa@gmail.com>
2023-04-17 22:56:52 +0300

    task 7

---
+++ Homework/task_07/script.js
@@ -0,0 +1,23 @@
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if (hours < 10) {
+    hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+    minutes = '0' + minutes;
+    }
+
+    if (seconds < 10) {
+    seconds = '0' + seconds;
+    }
+
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+
+    
